e16365a8ae1cd3c0f5da16dee14686ea49a23a16
Alejandro Bece <alejandro.becerra.pro@gmail.com>
2024-09-18 13:00:37 -0500

    creation widgets maquetation for projects

---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          leading: const Padding(
+            padding: EdgeInsets.all(4.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://assets0.setlisting.cdn.crowds.dk/artists/4ea1da5d349f745fa0000545/miranda-cosgrove-46bab93225.png'),
+            ),
+          ),
+          title: const Text('Mi Amor')),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return (index % 2 == 0)
+                    ? const HerMessageBubble()
+                    : const MyMessageBubble();
+              },
+            )),
+            //caja de mensajes
+            const MessageFieldBox(),
+          ],
+        ),
+      ),
+    );
+  }
+}
